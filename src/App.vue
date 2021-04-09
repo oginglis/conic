3fd067c6ac0fb3150e7eb12c7e94c71954af2c0a
Oliver Inglis <ollie@theinglistwins.com>
2021-04-09 16:48:33 +0100

    new worK

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,77 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="mini-val-d"> d  = {{dValuedeg}} degrees {{dValuerad}} radians (translational distance in z axis)</h1>
+    <h1 class="mini-val-a"> a  = {{aValuedeg}} degrees {{aValuerad}} radians (angle of rotation in yz plane)</h1>
+
+    <div class="viz-wrapper">
+      <HelloWorld msg="Welcome to Your Vue.js App" v-on:sendPoints="updateCoordinates"/>
+      <Plotly :data="data" :layout="layout" :display-mode-bar="false"></Plotly>
+    </div>
+    <table >
+      <tr>
+        <th></th>
+        <th>X</th>
+        <th>Y</th>
+      <tr v-for="(item, index) in fullCord[0]" :key="item.id">>
+        <td>{{item.toFixed(2)}}</td>
+        <td>{{fullCord[1][index].toFixed(2)}}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import { Plotly } from 'vue-plotly';
+
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Plotly
+  },
+  computed: {
+    fullCord: function () {
+      return [this.data[0].x , this.data[0].y]
+    }
+  },
+  data: function(){
+    return {
+      aValuedeg: 90,
+      dValuedeg: 0,
+      aValuerad: 1.57,
+      dValuerad: 0,
+      data:[{
+        x: [],
+        y: [],
+        type:"scatter"
+      }],
+      layout:{
+        title: "Conic Section",
+        xaxis: {
+          range: [-250, 250],
+          type: 'linear'
+        },
+        yaxis: {
+          range: [-250, 250],
+          type: 'linear'
+        }
+      },
+      config: {responsive: true}
+    }
+  },
+  methods: {
+    updateCoordinates: function (coor) {
+
+      this.data[0].y = coor[0];
+      this.data[0].x = coor[1];
+      this.aValuedeg = coor[2].toFixed(2);
+      this.dValuedeg = coor[3].toFixed(2);
+      this.aValuerad = coor[4].toFixed(2);
+      this.dValuerad= coor[5].toFixed(2);
+    }
   }
 }
 </script>
@@ -23,5 +84,33 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
+}
+
+.viz-wrapper {
+  display: flex;
+  justify-content: space-around;
+  width: 80%;
+  margin: 0 auto;
+}
+
+table {
+  width: 500px;
+  font-size: 10px;
+  margin: 0 auto;
+  border: 1px solid black;
+}
+
+.mini-val-d {
+  position: absolute;
+  font-size: 14px;
+  top: 0px;
+  left: 100px;
+}
+.mini-val-a {
+  position: absolute;
+  font-size: 14px;
+  top: 20px;
+  left: 100px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,272 +11,155 @@ export default {
   },
   data() {
     return {
-
+      line_2d_s: [],
+      line_2d_t: [],
+      aLabeldeg: 0,
+      dLabeldeg: 90,
+      dLabelrad: 0,
+      aLabelrad: 0,
     };
   },
+  methods: {
+    sendPoints() {
+      this.$emit("sendPoints", [this.line_2d_s, this.line_2d_t, this.aLabeldeg, this.dLabeldeg, this.aLabelrad, this.dLabelrad, ]);
+    }
+  },
   mounted() {
-    const sketch = (p5) => {
+
+    const sketch=(p5)=> {
       // These are your typical setup() and draw() methods
       /* eslint-disable no-param-reassign */
-      class Ball {
-        constructor() {
-          this.x = 200;
-          this.y = 300;
-          this.vx = 0;
-          this.vy = 0;
-          this.size = 20;
-          this.pulled = false;
-          this.fired = false;
-          this.c = p5.color(255, 204, 0);
-          this.distance = 0;
-          this.mass = 50;
+      let slider, slider2, a, d, C, S, A, c, E;
+      // let line_2d_s = []
+      // let line_2d_t = []
+      let squareSize = 300;
 
-          this.lastX = 0;
-          this.lastY = 0;
-          this.angle = 0;
-        }
+      p5.setup = () =>{
+        p5.createCanvas(500, 500, p5.WEBGL);
+        createAllSliders();
+        p5.angleMode( p5.RADIANS );
+      //   assign inital values
+        d = p5.radians(slider.value());
+        a = p5.radians(slider2.value());
+        C = p5.cos(a);
+        S = p5.sin(a);
+      }
 
+      p5.draw = ()=> {
+        p5.background(220);
+        p5.normalMaterial();
+        p5.rectMode(p5.CENTER);
 
-        setColor (){
-          // Sets ball colour depending on if ball clicked
-          this.pulled ? this.c = p5.color(255, 204, 110) : this.c = p5.color(0,62,116);
-        }
+        //
+        d = p5.radians(slider.value());
+        a = p5.radians(slider2.value());
+        C = p5.cos(a);
+        S = p5.sin(a);
 
-        display() {
-          // Set ball colour
-          this.setColor();
-          // Set up ball colour and styles with this ball colour
-          p5.fill(this.c);
-          p5.noStroke();
-          // Draws ellipse using X,Y, and size variables
-          p5.imageMode(p5.CENTER);
-          // p5.rotate(this.angle)
-          p5.image(img, this.x, this.y, this.size, this.size*1.4);
-          // p5.ellipse(this.x, this.y, this.size * 2);
-          // Changes colour to black for text
-          p5.fill(p5.color(0, 0, 0));
-          // Displays the ball mass on top of the ball
-          p5.text( p5.round(ball.mass, 2) + "kg", ball.x +50 , ball.y -10);
-        }
+        p5.push();
+        p5.translate(0, slider.value(), 0);
+        p5.rotateX(convertSlider(slider2));
+        p5.square(0, 0, squareSize);
+        p5.pop();
+        p5.stroke(126);
+        p5.line(0, 0, 240, 0);
+        p5.line(0, 0, 0, -240);
+        p5.stroke(255);
+        p5.line(0, 0, -240, 0);
+        p5.line(0, 0, 0, 240);
+        p5.push();
+        p5.translate(0, -75)
+        p5.cone(70, 150, 24);
+        p5.pop();
+        p5.push();
+        p5.translate(0, 75);
+        p5.angleMode(p5.DEGREES);
+        p5.rotateZ(180);
+        p5.cone(70, 150, 24);
+        p5.pop();
+      }
 
-        clicked() {
-          // Test to see if ball is clicked
-          if ( p5.dist(p5.mouseX, p5.mouseY, this.x, this.y) < this.size) {
-            // Set the pulled variable to true if it is clicked
-            this.pulled = true;
-          }
-          // Otherwise return false as the ball is not clicked
-          return false;
-        }
+      function convertSlider(sl) {
+        return sl.value() - 90
+      }
 
-        update () {
-          // Test to see if ball is dragged
-          if (this.pulled) {
-            if (p5.dist(p5.mouseX, p5.mouseY, this.x, this.y) < 2 * this.size ) {
-              this.x = p5.constrain(p5.mouseX, p5.windowWidth / 3.5, p5.windowWidth / 1.4);
-              this.y = p5.constrain(p5.mouseY, 300, 399);
-            }
-          }
+      function createAllSliders() {
+        slider = p5.createSlider(-180, 180, 0, .01);
+        slider.position(10, 10);
+        slider.style('width', '80px');
+        slider.input(resetPoints);
 
-          if (this.fired && this.y !== (p5.windowHeight)){
-
-
-            //Velocity in Y Axis
-            this.vy += g;
-            // Positon - Velocity in Y and Mass
-            this.y -= this.vy / this.mass
-
-      //        Velocity X Axis
-            if (this.vx === 0) {
-              this.vx = p5.windowWidth / 2 - this.x;
-              console.log(this.vx);
-            }
-
-            this.x += this.vx / this.mass
-
-            // update rotation
-
-            let v1 = p5.createVector(this.lastX, this.lastY, 0);
-            let v2 = p5.createVector(this.x, this.y, 0);
-
-            this.angle = p5.degrees(v1.angleBetween(v2));
-            // angle is PI/2
-
-
-            this.lastX = this.x
-            this.lasyY = this.y
-            if (this.y > p5.windowHeight){
-              this.y = p5.windowHeight;
-              setTimeout(resetSketch, 500);
-            }
-          }
-        }
+        slider2 = p5.createSlider(0, 180, 90, .01);
+        slider2.position(10, 30);
+        slider2.style('width', '80px');
+        slider2.input(resetPoints);
 
       }
-      // Catpult Still
-      class Rope {
-        constructor(b){
-          this.ballx = b.x;
-          this.bally = b.y;
-          this.post1 = p5.windowWidth / 3.5;
-          this.post2 = 300;
-          this.post3 = p5.windowWidth / 1.4;
-          this.post4 = 300;
+
+      const resetPoints = () => {
+        this.line_2d_s = [];
+        this.line_2d_t = [];
+        this.aLabeldeg = p5.degrees(a);
+        this.dLabeldeg = p5.degrees(d);
+        this.aLabelrad = a;
+        this.dLabelrad = d;
+        let i;
+        for (i = -250; i < 250; i++){
+          this.line_2d_s.push(p5.degrees(l_s(i)));
+          this.line_2d_t.push(p5.degrees(l_t(i)));
         }
+        this.sendPoints();
+      }
 
-        update () {
 
+      function l_t(alpha) {
+        if (d == 0 && a == 0) {
+          return 0
+        } else if (d == 0 && a != 0) {
+          return d * p5.cos(alpha)
+        } else if (0 <= a && a < p5.PI / 4) {
+          return d * p5.cos(alpha)
+        } else if (a == p5.PI / 4) {
+          return alpha
+        } else {
+          A = 0.5 * d * (-1 / (S + C) - p5.sin(p5.PI / 4) / p5.sin(p5.PI / 4 + a));
+          c = p5.sin(p5.PI / 4) / p5.sin(p5.PI / 4 + a) + A
+          E = c / A
+          return p5.cos(alpha) * (A / (1 - E * p5.sin(alpha)))
         }
+      }
 
-        display (b) {
-          if (b.y < initY  ) {
-      //       Draw Straight Line
-            p5.fill(0);
-            p5.stroke(126);
-            p5.line(this.post1, this.post2, this.post3, this.post4);
-            p5.ellipse(this.post1, this.post2, 7, 7);
-            p5.ellipse(this.post3, this.post4, 7, 7);
-          } else if (b.fired && b.vy < 0) {
-              p5.fill(0);
-              p5.stroke(126);
-              p5.line(this.post1, this.post2, this.post3, this.post4);
-              p5.ellipse(this.post1, this.post2, 7, 7);
-              p5.ellipse(this.post3, this.post4, 7, 7);
-          }
-            else       {
-            p5.fill(0);
-            p5.stroke(126);
-      //     First String
-            p5.line(b.x, b.y, this.post1, this.post2);
-            p5.ellipse(this.post1, this.post2, 7, 7);
-      //     Second String
-            p5.line(b.x, b.y, this.post3, this.post2);
-            p5.ellipse(this.post3, this.post4, 7, 7);
-          }
+      function l_s(alpha){
+        if(d==0 && a==0){
+          return 0
+        } else if ( d==0 && a!=0){
+          return alpha
+        } else if (0<= a && a< p5.PI/4){
+          let r1 = d*p5.sin(p5.PI/4)/(p5.sin(p5.PI/4 + a))
+          let r2 = d*p5.sin(p5.PI/4)/(p5.sin(p5.PI/4 - a))
+          return -r1 + r2 + 0.5*(r1 + r2)*p5.sin(alpha)
+        } else if (a == p5.PI/4 ) {
+          return alpha**2
+        } else {
+          A = 0.5*d*(-1/(S+C) -p5.sin(p5.PI/4)/p5.sin(p5.PI/4 + a));
+          c = p5.sin(p5.PI/4)/p5.sin(p5.PI/4 + a) + A
+          E = c/A
+          return p5.sin(alpha)*(A/(1-E*p5.sin(alpha)));
 
         }
       }
 
-      class Tracer {
-        constructor () {
-          this.dots = []
-        }
-
-        display(b){
-          if (b.fired){
-            let xdot = b.x;
-            let ydot = b.y;
-            this.dots.push([xdot, ydot]);
-            this.dots.forEach(d => {
-              p5.ellipse(d[0], d[1], 2);
-
-            })
-          } else {
-            return
-          }
-        }
-      }
-
-      // Canvas size constants
-
-      // Init ball positon & size constants
-      const initY = 300;
-
-      // Physics constants
-      const g = -9.80;
-
-      // Initialize the Variables
-
-      let slider;
-      let ball;
-      let rope;
-      let tracer;
-
-      let img;
-      p5.preload = () => {
-        img = p5.loadImage(require('@/assets/rocket.png'));
-      }
-      p5.setup = () => {
-         // Create canvas and set frame rate
-        p5.createCanvas(p5.windowWidth, p5.windowHeight);
-        p5.frameRate(200);
-
-
-
-        ball = new Ball ( );
-        ball.x = p5.windowWidth /2;
-        ball.y = initY;
-        rope = new Rope (ball);
-        tracer = new Tracer (ball)
-        p5.line(30, 20, 85, 75);
-        let button = p5.createButton("Reset Catapult");
-        button.position(10,10);
-        button.mousePressed(resetSketch);
-        button.style('position', 'fixed');
-        slider = p5.createSlider(1, 20, 10, 1);
-        slider.position(10, 40);
-        slider.style('width', '100px');
-        slider.style('position', 'fixed');
-      };
-      p5.draw = () => {
-
-        p5.background(212,239,252);
-        p5.text( 'Ball Mass = ' + slider.value() + ' kg' , 120, 55);
-        ball.update();
-        rope.display(ball);
-        tracer.display(ball);
-        ball.display();
-        p5.fill(p5.color(0,0,0));
-        p5.text( 'Ball Velocity ' + p5.round(ball.vy, 2), 10, 90);
-        ball.mass = slider.value();
-        ball.size = p5.map(slider.value(), 1, 20, 50, 120);
-      };
-
-      p5.mousePressed = () => {
-        if (ball.clicked()) {
-          ball.pulled = true;
-          ball.fired = false;
-        }
-      }
-
-      p5.mouseReleased = () => {
-        if (ball.pulled) {
-          // ball.vy = dist(200, 200, ball.x, ball.y);
-          ball.vy = (ball.y - initY) * 2.5;
-          ball.pulled = false;
-          ball.fired = true;
-        }
-      };
-
-      function resetSketch(){
-        ball.x = p5.windowWidth /2 ;
-        ball.y = initY;
-        ball.pulled = false;
-        ball.fired = false;
-        ball.vy = 0;
-        ball.vx = 0;
-        tracer.dots = [];
-        ball.update();
-        rope.display(ball);
-        tracer.display(ball);
-        ball.display();
-      }
-
-      p5.windowResized = () =>{
-        p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
-      }
-    };
+    }
     // Attach the canvas to the div
     /* eslint-disable no-new */
     new P5(sketch, 'canvas');
+    this.$emit('incrementBy', 5);
   },
 };
 </script>
 
 <style>
 #canvas {
-  position: absolute;
-  top: 0;
-  left: 0;
+
 }
 </style>
